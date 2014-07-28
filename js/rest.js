cd4dee0615e3566e29c9d3cf869359437ada3266
Tushar <tushar@Macintosh.local>
2014-07-28 20:10:59 +0530

    API compressed

--- js/rest.js
+++ js/rest.js
@@ -1,60 +1,60 @@
+var apiServer = 'http://localhost/politician/index.php';
 var restservice = angular.module('restservice', [])
-
 
 .factory('RestService', function ($http) {
    
     return {
             viewall: function(){
                 console.log();
-                return $http.get("http://localhost/politician/index.php/vote/create");
+                return $http.get(apiServer+"/vote/create");
             },
         region: function () {
-            return $http.get("http://localhost/politician/index.php/region/find",{});
+            return $http.get(apiServer+"/region/find",{});
         },
         mpconstituency: function () {
-            return $http.get("http://localhost/politician/index.php/mpconstituency/find",{});
+            return $http.get(apiServer+"/mpconstituency/find",{});
         },
         mlaconstituency: function () {
-            return $http.get("http://localhost/politician/index.php/mlaconstituency/find",{});
+            return $http.get(apiServer+"/mlaconstituency/find",{});
         },
         image: function () {
-            return $http.get("http://localhost/politician/index.php/image/find",{});
+            return $http.get(apiServer+"/image/find",{});
         },
         position: function () {
-            return $http.get("http://localhost/politician/index.php/position/find",{});
+            return $http.get(apiServer+"/position/find",{});
         },
         video: function () {
-            return $http.get("http://localhost/politician/index.php/video/find",{});
+            return $http.get(apiServer+"/video/find",{});
         },
         state: function () {
-            return $http.get("http://localhost/politician/index.php/state/find",{});
+            return $http.get(apiServer+"/state/find",{});
         },
         mpconstituency: function () {
-            return $http.get("http://localhost/politician/index.php/mpconstituency/find",{});
+            return $http.get(apiServer+"/mpconstituency/find",{});
         },
         inputdetails: function (id,firstname,lastname) {
             console.log(id+firstname+lastname);
-            return $http.get("http://localhost/politician/index.php/vote/create?id="+id+"&firstname="+firstname+"&lastname="+lastname,{});
+            return $http.get(apiServer+"/vote/create?id="+id+"&firstname="+firstname+"&lastname="+lastname,{});
         },
         deletecustomer: function(id){
         console.log(id);
-            return $http.get("http://localhost/starting/index.php/welcome/deletecustid?id="+id,{});
+            return $http.get(apiServer+"/welcome/deletecustid?id="+id,{});
         },
         insertmembership: function(data){
     console.log(data);
-            return $http.get("http://localhost/politician/index.php/membership/create",{params:data});
+            return $http.get(apiServer+"/membership/create",{params:data});
     },
         insertuserinfo: function(data){
     console.log(data);
-            return $http.get("http://localhost/politician/index.php/registration/create",{params:data});
+            return $http.get(apiServer+"/registration/create",{params:data});
     },
         insertvote: function(data){
     console.log(data);
-            return $http.get("http://localhost/politician/index.php/vote/create",{params:data});
+            return $http.get(apiServer+"/vote/create",{params:data});
     },
         viewid: function(id){
         console.log(id);
-            return $http.get("http://localhost/politician/index.php/vote/create?id="+id,{});
+            return $http.get(apiServer+"/vote/create?id="+id,{});
         }
     }
 });
